c5c7c675053490729607b185949f9cc185e1842a
Vincent Batts <vbatts@redhat.com>
2013-11-29 22:51:28 -0500

    trying to add the effect handler

--- sox.go
+++ sox.go
@@ -37,9 +37,44 @@ func Quit() int {
 }
 
 /*
+Search for effect by name. nil is returned if nothing found.
+*/
+func FindEffect(name string) *Effect {
+	return newEffect(C.sox_find_effect(C.CString(name)))
+}
+
+func newEffect(eh *C.sox_effect_handler_t) *Effect {
+	if eh == nil {
+		return nil
+	}
+	return &Effect{
+		Name:   C.GoString(eh.name),
+		Usage:  C.GoString(eh.usage),
+		effect: eh,
+	}
+}
+
+type Effect struct {
+	Name   string
+	Usage  string
+	Flags  uint
+	effect *C.sox_effect_handler_t
+}
+
+func (e *Effect) cEffect() (eh *C.sox_effect_handler_t) {
+	if e.effect != nil {
+		eh = e.effect
+	}
+	eh.name = C.CString(e.Name)
+	eh.usage = C.CString(e.Usage)
+	eh.flags = C.uint(e.Flags)
+	return eh
+}
+
+/*
 Returns the table of format handler names and functions.
 */
-func FormatFns() (hurr []string) {
+func formatFns() (hurr []string) {
 	var format_tabs *C.sox_format_tab_t
 
 	format_tabs = C.sox_get_format_fns()
@@ -49,7 +84,22 @@ func FormatFns() (hurr []string) {
 	return
 }
 
-type FormatTab struct {
+type Format struct {
+}
+
+func (f *Format) AddEffect(e Effect) error {
+	return nil
+}
+
+func (f *Format) Read(p []byte) (n int, err error) {
+	return
+}
+
+func (f *Format) Close() (err error) {
+	return nil
+}
+
+type formatTab struct {
 	Name string
 	fn   C.sox_format_fn_t
 }
--- sox_test.go
+++ sox_test.go
@@ -1,21 +1,28 @@
 package sox
 
 import (
-  //"fmt"
-  "testing"
+	"fmt"
+	"testing"
 )
 
 func TestInit(t *testing.T) {
-  i := FormatInit()
-  if i != 0 {
-    t.Errorf("FormatInit() returned: %d", i)
-  }
-  defer FormatQuit()
+	i := FormatInit()
+	if i != 0 {
+		t.Errorf("FormatInit() returned: %d", i)
+	}
+	defer FormatQuit()
 }
 
 func TestFormats(t *testing.T) {
-  Init()
-  FormatFns()
-  defer Quit()
+	Init()
+	formatFns()
+	defer Quit()
 
+}
+func TestEffects(t *testing.T) {
+	e := FindEffect("vol")
+	if e == nil {
+		t.Errorf("should not be nil")
+	}
+	fmt.Printf("%#v\n", e)
 }
